ce0fbcda00ff78b1c65659f38d43bfdaec15ad2f
Snmoyano <sergio.nicolas.moyano@gmail.com>
2022-10-18 19:23:51 -0300

    3commit

--- src/components/pokedexId/Pokemon404.jsx
+++ src/components/pokedexId/Pokemon404.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import gif from "../home/DEUt.gif";
+import { Header } from "../shared/Header";
+import "./styles/pokemon404.css";
 
 const Pokemon404 = () => {
-    return (
-        <>
-            <h1>Pokemon not found  </h1>
-            <Link to='/pokedex' >Return to Pokedex</Link>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <div className="container__error">
+        <div className="error-title">
+          <Link className="error-title-link" to="/pokedex">
+            Return to Pokedex
+          </Link>
+        </div>
+        <div className="error__img">
+          <img src={gif} alt="" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Pokemon404 
+export default Pokemon404;
